ec6cd82558069004a4da27120a454461e714ad6c
omarsalem7 <omarsalem0721@gmail.com>
2022-03-01 17:52:19 +0200

    create and remove books added using redux

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
-const Book = ({ title, author }) => (
-  <div>
-    <h2>{title}</h2>
-    <h5>{author}</h5>
-    <button type="button">Remove</button>
-  </div>
-);
+const Book = ({ id, title, catagory }) => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h5>{catagory}</h5>
+      <button id={id} type="button" onClick={() => dispatch(removeBook(id))}>
+        Remove
+      </button>
+    </div>
+  );
+};
 
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  catagory: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
 
