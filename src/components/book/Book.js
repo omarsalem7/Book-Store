35eeacb623407d22e2ae0b9e397c922e1c804a3c
omarsalem7 <omarsalem0721@gmail.com>
2022-03-02 20:45:37 +0200

    Connect with api

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -3,13 +3,22 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../../redux/books/books';
 
-const Book = ({ id, title, catagory }) => {
+const Book = ({ id, title, category }) => {
   const dispatch = useDispatch();
+  const removeHandler = () => {
+    fetch(
+      `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/4D7y39UkNgaQrWvdHrKq/books/${id}`,
+      {
+        method: 'DELETE',
+      },
+    );
+    dispatch(removeBook(id));
+  };
   return (
     <div>
       <h2>{title}</h2>
-      <h5>{catagory}</h5>
-      <button id={id} type="button" onClick={() => dispatch(removeBook(id))}>
+      <h5>{category}</h5>
+      <button id={id} type="button" onClick={removeHandler}>
         Remove
       </button>
     </div>
@@ -19,7 +28,7 @@ const Book = ({ id, title, catagory }) => {
 Book.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  catagory: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
   books: booksReducer,
 });
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 export default store;
